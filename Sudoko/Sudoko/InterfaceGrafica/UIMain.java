ad9f180a36d713d5e12f765af9583935c7def139
Kaue Braz Da Silveira <kaue.silveira.423@gmail.com>
2025-05-08 23:05:27 -0300

    Add files via upload
    Sudoku

---
+++ Sudoko/Sudoko/InterfaceGrafica/UIMain.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import br.com.dio.ui.custom.screen.MainScreen;
+
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
+
+public class UIMain {
+
+    public static void main(String[] args) {
+        final var gameConfig = Stream.of(args)
+                .collect(toMap(k -> k.split(";")[0], v -> v.split(";")[1]));
+        var mainsScreen = new MainScreen(gameConfig);
+        mainsScreen.buildMainScreen();
+    }
+
+}
