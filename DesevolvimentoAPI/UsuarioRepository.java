9d8b9392332a7033947b7a5424032a61e743ed9c
Kaue Braz Da Silveira <kaue.silveira.423@gmail.com>
2025-05-23 19:24:49 -0300

    Delete DesevolvimentoAPI directory

--- DesevolvimentoAPI/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.apirest.repository;
-
-import com.example.apirest.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
